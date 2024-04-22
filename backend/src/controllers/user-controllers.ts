be89cb766b4dd430c1272fbf0096e244f0da9445
Diogo Meira <diogomeira81@gmail.com>
2024-04-22 18:54:01 +0100

    secure: true, sameSite: none

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -105,8 +105,8 @@ export const userLogin = async (req: Request, res: Response, next: NextFunction)
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
             httpOnly: true,
             signed: true,
-            sameSite:'lax',
-            // secure:true,
+            sameSite:'none',
+            secure:true,
             domain: 'chat-bot-q0rp.onrender.com'
         })
 
