0b9058b5702eff9f343053183dea21cd827eecee
Diogo Meira <diogomeira81@gmail.com>
2024-04-22 18:34:59 +0100

    sameSite: lax removed secure true

--- backend/dist/controllers/user-controllers.js
+++ backend/dist/controllers/user-controllers.js
@@ -69,8 +69,8 @@ export const userLogin = async (req, res, next) => {
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
             httpOnly: true,
             signed: true,
-            sameSite: 'none',
-            secure: true,
+            sameSite: 'lax',
+            // secure:true,
             domain: 'chat-bot-q0rp.onrender.com'
         });
         return res.status(200).json({ message: 'User logged in sucessfully', name: user.name, email: user.email });
